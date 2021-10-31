7d1703b834c91625108e4892abbc5c3586dda742
Kyungjoo Ha <429why@gmail.com>
2021-11-01 01:27:49 +0900

    add: Color variables

--- src/components/utils/_var.tsx
+++ src/components/utils/_var.tsx
@@ -1,21 +1,38 @@
 import { createGlobalStyle } from 'styled-components';
 
+/*
+#fff0edGrey
+#fd6b6bPink
+#544e5aGrey
+#a15b54 Brown
+#7fac67Green
+#a661a9Violet
+#df723dOrange
+#ca9650 Brown
+#fcb022Orange
+#d50d15red
+*/
+
 const Colors = {
   black: '#303030',
-  darkGray: '#575757',
+  darkGray: '#544e5a',
   gray: '#7a7a7a',
   mediumGray: '#a7a7a7',
   lightGray: '#e0e2e3',
-  lightYellow: '#ffcd01',
-  yellow: '#ffba01',
-  darkYellow: '#ffba01'
+  pink: '#fd6b6b',
+  // pinkGray: '#fff0ed',
+  pinkGray: 'white',
+  brown: '#a15b54'
 };
 
 const GlobalStyle = createGlobalStyle`
   .title {
-    margin: 2rem auto 1rem;
+    margin: 0 auto;
+    padding: 2rem;
     text-align: center;
     font-size: 3.5rem;
+    text-transform: uppercase;
+    font-family: 'Anton';
   }
 `;
 
