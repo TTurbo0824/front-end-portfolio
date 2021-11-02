5715bcf165553453ed545c977cad430d4f59324c
Kyungjoo Ha <429why@gmail.com>
2021-11-02 12:24:58 +0900

    modify: Color variables

--- src/components/utils/_var.tsx
+++ src/components/utils/_var.tsx
@@ -1,28 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 
-/*
-#fff0edGrey
-#fd6b6bPink
-#544e5aGrey
-#a15b54 Brown
-#7fac67Green
-#a661a9Violet
-#df723dOrange
-#ca9650 Brown
-#fcb022Orange
-#d50d15red
-*/
-
 const Colors = {
   black: '#303030',
   darkGray: '#544e5a',
   gray: '#7a7a7a',
   mediumGray: '#a7a7a7',
   lightGray: '#e0e2e3',
-  pink: '#fd6b6b',
-  // pinkGray: '#fff0ed',
-  pinkGray: 'white',
-  brown: '#a15b54'
+  backgroundColor: '#252525',
+  beige: '#fefaef'
 };
 
 const GlobalStyle = createGlobalStyle`
