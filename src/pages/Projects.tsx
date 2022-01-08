c0b42dd7b13a2c7ccfb3b56cf668482b92e7b531
Kyungjoo Ha <429why@gmail.com>
2022-01-08 17:33:45 +0900

    add: New project

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -95,16 +95,22 @@ const ProjectWrapper = styled.div`
   }
   .line:last-child {
     padding-top: 0.5rem;
-    /* background-color: forestgreen; */
   }
 `;
 
-// ==================================================================
-//                               TO DO
-// ==================================================================
-
 function Projects() {
   const handleDragStart = (e: any) => e.preventDefault();
+
+  const WalkingDogList = ['walkingdog_1', 'walkingdog_2', 'walkingdog_3', 'walkingdog_4'];
+  const WalkingDogItems = WalkingDogList.map((image, idx) => (
+    <img
+      src={`https://tturbo0824.github.io/front-end-portfolio/images/projects/${image}`}
+      key={idx}
+      className="img"
+      onDragStart={handleDragStart}
+    />
+  ));
+
   const M4MList = ['m4m_1', 'm4m_2', 'm4m_3', 'm4m_4', 'm4m_5'];
   const M4MItems = M4MList.map((image, idx) => (
     <img
@@ -135,26 +141,65 @@ function Projects() {
     />
   ));
 
-  const GuessEmojiList = [
-    'guessemoji_1.png',
-    'guessemoji_4.gif',
-    'guessemoji_2.png',
-    'guessemoji_3.png'
-  ];
-  const GuessEmojiItems = GuessEmojiList.map((image, idx) => (
-    <img
-      src={`https://tturbo0824.github.io/front-end-portfolio/images/projects/${image}`}
-      key={idx}
-      className="img"
-      onDragStart={handleDragStart}
-    />
-  ));
-
   return (
     <ProjectWrapper>
       <GlobalStyle />
       <div className="main">
         <div className="title">Projects</div>
+        <div className="container">
+          <div className="card">
+            <div className="field">WalkingDog</div>
+            <div className="image-container">
+              <AliceCarousel mouseTracking infinite={true} items={WalkingDogItems} />
+            </div>
+            <div className="content">
+              <div className="line">방문 도그워커 검색 및 반려견 산책 서비스</div>
+              <div className="line">프로젝트 기간: 2021.10 - 2022.1 (1인 / 10주 개인 프로젝트)</div>
+              <div className="line">
+                <a
+                  href="http://walkingdog.site"
+                  className="link"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  🔗 배포 링크
+                </a>
+                |
+                <a
+                  href="https://github.com/TTurbo0824/WalkingDog"
+                  className="link"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  GitHub
+                </a>
+                |
+                <a
+                  href="https://github.com/TTurbo0824/WalkingDog/wiki"
+                  className="link"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  기획서
+                </a>
+              </div>
+              <div className="line">업무내용</div>
+              <ul>
+                <li>
+                  프론트엔드: 랜딩 페이지, 메인 페이지, 로그인/회원가입 모달, 회원 전용 페이지,
+                  도그워커 페이지 구현
+                </li>
+                <li>
+                  백엔드: 로그인/회원가입 관련 기능, 체험하기 기능, 회원 정보 관련 기능, 도그워커
+                  조회/검색 기능, 반려견 산책 요청/취소 기능, 이용 서비스 평점/후기 등록/수정/삭제
+                  기능
+                </li>
+                <li>배포: EC2, S3, CloudFront 등 AWS 서비스를 통한 배포</li>
+              </ul>
+              <div className="line">
+                사용 스택: React, Redux, styled-components, Node.js, Express.js, MySQL, Sequelize
+                ORM, JWT, OAuth 2.0, AWS (EC2, RDS, S3, Route 53)
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="container">
           <div className="card">
             <div className="field">Music for Millennials (M4M)</div>
@@ -303,57 +348,13 @@ function Projects() {
               </div>
               <div className="line">담당업무</div>
               <ul>
-                <li>프론트엔드: 로그인 모달, 회원 가입 모달, 회원 정보 페이지, To Do 추가/편집/삭제 기능 구현</li>
+                <li>
+                  프론트엔드: 로그인 모달, 회원 가입 모달, 회원 정보 페이지, To Do 추가/편집/삭제
+                  기능 구현
+                </li>
                 <li>백엔드: To Do 편집 기능 구현</li>
               </ul>
               <div className="line">사용 스택: TypeScript, React, Redux, styled-components</div>
-            </div>
-          </div>
-        </div>
-        <div className="container">
-          <div className="card">
-            <div className="field">Guess the Emoji</div>
-            <div className="image-container">
-              <AliceCarousel mouseTracking infinite={true} items={GuessEmojiItems} />
-            </div>
-            <div className="content">
-              <div className="line">
-                사용자의 코로 주어진 질문지에 해당하는 이모티콘을 찾는 얼굴인식 기술을 이용한 사고력
-                게임
-              </div>
-              <div className="line">프로젝트 기간: 2020.11 - 2020.12 (1인 / 3주 개인 프로젝트)</div>
-              <div className="line">
-                <a
-                  href="https://guess-the-emoji.glitch.me"
-                  className="link"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  🔗 배포 링크
-                </a>
-                |
-                <a
-                  href="https://github.com/TTurbo0824/p5_games/tree/master/guess_the_emoji"
-                  className="link"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  GitHub
-                </a>
-                |
-                <a
-                  href="https://hakyungjoo.com/guess_the_emoji.html"
-                  className="link"
-                  target="_blank"
-                  rel="noopener noreferrer">
-                  블로그
-                </a>
-              </div>
-              <div className="line">업무내용</div>
-              <ul>
-                <li>게임개발: p5.js를 이용한 인터페이스 디자인 및 프로그래밍</li>
-                <li>얼굴인식: ml5.js PoseNet을 이용한 머신러닝 얼굴인식 기능 구현</li>
-                <li>배포: Glitch를 통한 배포</li>
-              </ul>
-              <div className="line">사용 스택: JavaScript, p5.js, ml5.js PoseNet</div>
             </div>
           </div>
         </div>
